f809d19884165037be17921903121049108faf29
EduardoGarcia935 <eduardo.agarcia117@gmail.com>
2022-06-01 12:22:49 -0300

    Feature Acabando o JS

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 let numAleatorio = Math.round(Math.random() * 100) +1
-
+var qtdPalpites = 0;
 let entrada = document.querySelector('#entrada')
 let btnSubmit = document.querySelector('.submit')
-
 let listaPalpites = document.querySelector('.listaPalpites')
-
-var qtdPalpites = 0;
-
 let resultado = document.querySelector('.resultado')
-
 let distancia = document.querySelector('.distancia')
+let label = document.querySelector('.label')
+
 
 
 
@@ -32,24 +29,26 @@ function verificar(){
         resultado.classList.add('acerto')
         resultado.innerHTML = 'Parabéns! Seu número está correto!'
         distancia.innerHTML = ''
+        resultado.style.visibility = 'visible'
+        btnReTry.style.display = 'inline'
     }
     else if(tentativa < numAleatorio){
         resultado.classList.add('erro')
         resultado.innerHTML = 'Errado !'
         distancia.innerHTML = 'Seu palpite está muito baixo'
+        resultado.style.visibility = 'visible'
 
     }else if(tentativa > numAleatorio){
         resultado.classList.add('erro')
         resultado.innerHTML = 'Errado !'
         distancia.innerHTML = 'Seu palpite está muito alto'
-        
+        resultado.style.visibility = 'visible'
     }
 
     qtdPalpites ++;
     entrada.value = null;
 
     if(qtdPalpites === 10 ){
-        label = document.querySelector('.label')
         label.style.display = 'none'
         btnSubmit.style.display = 'none'
 
@@ -62,13 +61,15 @@ function verificar(){
 function reTry(){
     entrada.style.display = 'inline'
     btnReTry.style.display = 'none'
-    resultado.style.display = 'none'
+    label.style.display = 'inline'
+    btnSubmit.style.display = 'inline'
+    resultado.style.visibility = 'hidden'
     qtdPalpites = 0;
+    ultimosPalpites = ""
+    distancia.innerHTML = ""
+    listaPalpites.innerHTML = null
+    numAleatorio = Math.round(Math.random() * 100) +1
 }
 
 btnSubmit.addEventListener('click', verificar)
 btnReTry.addEventListener('click', reTry)
-
-
-
-console.log(numAleatorio)
